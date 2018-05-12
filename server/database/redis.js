d3edf3089395efd9b7ff4b65fe6ef08dbd3cf06e
hchesnutt <t.henry.chesnutt@gmail.com>
2018-05-11 20:24:28 -0700

    some refactor, configure redis, mongo to docker env vars

--- server/database/redis.js
+++ server/database/redis.js
@@ -1,9 +1,9 @@
 const redis = require('redis');
 
-const { HOST } = require('../server.js')
-
+const host = process.env.REDIS_HOST || 'localhost';
+console.log('process.env.REDIS_HOST: ', process.env.REDIS_HOST);
 // const client = redis.createClient('6379', HOST);
-const client = redis.createClient('6379', 'redis');
+const client = redis.createClient('6379', host);
 
 client.on('error', function (err) {
   console.log(err);
